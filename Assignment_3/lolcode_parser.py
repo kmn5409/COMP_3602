cb10167b0f6c592b26afd1c879e990fac494b176
kmn5409 <nichols.keanu9@gmail.com>
2019-11-20 16:34:14 -0400

    More expression

--- Assignment_3/lolcode_parser.py
+++ Assignment_3/lolcode_parser.py
@@ -23,14 +23,28 @@ grammar = Grammar(
     declaration  = ("I HAS A" label) / ("I HAS A" label "ITZ" label)
     comment      = ("BTW" string) / ("OBTW" string "TLDR")
     print_block  = ("VISIBLE" expression "MKAY?") / (expression expression)
-    if_block     = ("O RLY?" nlc "YA RLY" nlc code_block nlc "OIC") / ("O RLY?" nlc "YA RLY" nlc code_block nlc else_if_block nlc "OIC")
-    else_if_block= ("MEBBE" expression nlc code_block nlc else_if_block) / ("NO WAI" nlc code_block) / ("MEBBE" expression nlc code_block)
+    if_block     = ("O RLY?" nlc "YA RLY" nlc code_block nlc "OIC") / 
+                   ("O RLY?" nlc "YA RLY" nlc code_block nlc else_if_block nlc "OIC")
+    else_if_block= ("MEBBE" expression nlc code_block nlc else_if_block) / 
+                   ("NO WAI" nlc code_block) / ("MEBBE" expression nlc code_block)
     input_block  = "GIMME" label
     assignment   = label "R" expression
-    expression   = equals / both / not_equals / atom
+    expression   = equals / both / not_equals / either / greater / less / 
+                   add / sub / mul / div / mod / not / atom
+
     equals       = "BOTH SAEM" expression "AN" expression
     not_equals   = "DIFFRINT" expression "AN" expression
     both         = "BOTH OF" expression "AN" expression
+    either       = "EITHER OF" expression "AN" expression
+    greater      = "BIGGR OF" expression "AN" expression
+    less         = "SMALLR OF" expression "AN" expression
+    add          = "SUM OF" expression "AN" expression
+    sub          = "DIFF OF" expression "AN" expression
+    mul          = "PRODUKT OF" expression "AN" expression
+    div          = "QUOSHUNT OF" expression "AN" expression
+    mod          = "MOD OF" expression "AN" expression
+    not          = "NOT" expression
+
     label        = ~"[a-zA-Z0-9]*"i
     atom         = "WIN" / "FAIL" / "NOOB" / (numbers "." numbers) / (numbers) / (string / quoted)
     quoted       = ~'"[^\"]+"' 
