459c4cde596c76ba6012e00ca8c61b1504fdfa79
kmn5409 <nichols.keanu9@gmail.com>
2019-11-19 21:31:25 -0400

    Added Assignment 3 Starting working on the parser

---
+++ Assignment_3/lolcode_parser.py
@@ -0,0 +1,23 @@
+from parsimonious.grammar import Grammar
+
+grammar = Grammar(
+    """
+    bold_text    = bold_open text bold_close
+    text         = ~"[A-Z 0-9]*"i
+    bold_open    = "(("
+    bold_close   = "))"
+    """
+    )
+
+print(grammar.parse('((bold stuff))'))
+
+
+grammar = Grammar(
+    """
+    program     = begin end 
+    begin       = "HAI "
+    end         = "KTHXBAI"
+    """
+    )
+
+print(grammar.parse('HAI KTHXBAI'))
