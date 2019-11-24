df4cc7b806ce044a3c9ba44c54af21619cd6f856
kmn5409 <nichols.keanu9@gmail.com>
2019-11-24 18:30:23 -0400

    Added spacing and comma

--- Assignment_3/lolcode_parser.py
+++ Assignment_3/lolcode_parser.py
@@ -19,42 +19,44 @@ grammar = Grammar(
     program      = "HAI" nlc code_block nlc "KTHXBAI"
     code_block   = statement / (statement nlc code_block)
     statement    = loop / declaration / comment / print_block / expression
-    loop         = "IM IN YR" label "WILE" expression nlc code_block nlc "IM OUTTA YR" label
-    declaration  = ("I HAS A" label) / ("I HAS A" label "ITZ" label)
-    comment      = ("BTW" string) / ("OBTW" string "TLDR")
-    print_block  = ("VISIBLE" expression "MKAY?") / (expression expression)
-    if_block     = ("O RLY?" nlc "YA RLY" nlc code_block nlc "OIC") / 
-                   ("O RLY?" nlc "YA RLY" nlc code_block nlc else_if_block nlc "OIC")
-    else_if_block= ("MEBBE" expression nlc code_block nlc else_if_block) / 
-                   ("NO WAI" nlc code_block) / ("MEBBE" expression nlc code_block)
-    input_block  = "GIMME" label
-    assignment   = label "R" expression
+    loop         = "IM IN YR " label "WILE " expression nlc code_block nlc "IM OUTTA YR " label
+    declaration  = ("I HAS A " label) / ("I HAS A " label "ITZ " label)
+    comment      = ("BTW " string) / ("OBTW " string " TLDR")
+    print_block  = ("VISIBLE " expression " MKAY?") / (expression expression)
+    if_block     = ("O RLY? " nlc " YA RLY" nlc code_block nlc " OIC") / 
+                   ("O RLY? " nlc " YA RLY" nlc code_block nlc else_if_block nlc " OIC")
+    else_if_block= ("MEBBE " expression nlc code_block nlc else_if_block) / 
+                   ("NO WAI " nlc code_block) / ("MEBBE " expression nlc code_block)
+    input_block  = "GIMME " label
+    assignment   = label " R " expression
     expression   = equals / both / not_equals / either / greater / less / 
                    add / sub / mul / div / mod / not / atom
 
-    equals       = "BOTH SAEM" expression "AN" expression
-    not_equals   = "DIFFRINT" expression "AN" expression
-    both         = "BOTH OF" expression "AN" expression
-    either       = "EITHER OF" expression "AN" expression
-    greater      = "BIGGR OF" expression "AN" expression
-    less         = "SMALLR OF" expression "AN" expression
-    add          = "SUM OF" expression "AN" expression
-    sub          = "DIFF OF" expression "AN" expression
-    mul          = "PRODUKT OF" expression "AN" expression
-    div          = "QUOSHUNT OF" expression "AN" expression
-    mod          = "MOD OF" expression "AN" expression
-    not          = "NOT" expression
+    equals       = "BOTH SAEM " expression " AN" expression
+    not_equals   = "DIFFRINT " expression "AN " expression
+    both         = "BOTH OF " expression "AN " expression
+    either       = "EITHER OF " expression "AN " expression
+    greater      = "BIGGR OF " expression "AN " expression
+    less         = "SMALLR OF " expression "AN " expression
+    add          = "SUM OF " expression "AN " expression
+    sub          = "DIFF OF " expression "AN " expression
+    mul          = "PRODUKT OF " expression "AN " expression
+    div          = "QUOSHUNT OF " expression "AN " expression
+    mod          = "MOD OF " expression "AN " expression
+    not          = "NOT " expression
 
     label        = ~"[a-zA-Z0-9]*"i
-    atom         = "WIN" / "FAIL" / "NOOB" / (numbers "." numbers) / (numbers) / (string / quoted)
+    atom         = "WIN " / "FAIL " / "NOOB " / (numbers "." numbers) / (numbers) / (string / quoted)
     quoted       = ~'"[^\"]+"' 
     numbers      = ~"[0-9]*"i
-    string       = ~"[a-zA-Z0-9]*"i 
+    string       = ~"[a-zA-Z0-9 ']*"
     nlc          = "\n" / ~","
     """
     )
 
-print(grammar.parse('''HAI,WIN,KTHXBAI'''))
+print(grammar.parse('''HAI
+BTW This is 'Hello'
+KTHXBAI'''))
 #Problem with normal string for atom
 
 '''
