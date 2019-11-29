61557613f1f2263864ce43bf07cdc06d91a89404
Shereece Victor <46582048+AlexS473@users.noreply.github.com>
2019-11-29 18:17:43 -0400

    Added exception handling
    Still runs in only debug mode, Accepts input file in command line

--- Assignment_3/parser.py
+++ Assignment_3/parser.py
@@ -1,8 +1,6 @@
-
 import argparse
 from arpeggio import *
-from arpeggio import RegExMatch as _ 
-
+from arpeggio import RegExMatch as _
 
 argument_parser = argparse.ArgumentParser()
 argument_parser.add_argument('--input')
@@ -12,113 +10,146 @@ argument_parser.add_argument('-debug', action='store_true')
 def programme():
     return hai, newlines, code_block, kthnxbye
 
+
 def hai():
     return _(r'HAI')
+
 
 def code_block():
     return [ZeroOrMore((statement, newlines)), statement, newlines]
 
+
 def statement():
     return [declaration, comment, input_block, assignment, print_block, expression]
+
 
 def declaration():
     return [(simple_declaration, decl_assignment), simple_declaration]
 
+
 def simple_declaration():
-    return "I", "HAS", "A", label 
+    return "I", "HAS", "A", label
+
 
 def decl_assignment():
     return "ITZ", value
 
+
 def comment():
     return [comment1]
+
 
 def comment1():
     return _(r'BTW'), ZeroOrMore(string_body)
 
+
 def print_block():
-    return "VISIBLE",expression, "MKAY?"
+    return "VISIBLE", expression, "MKAY?"
+
 
 def input_block():
     return "GIMMEH", label
 
+
 def assignment():
     return label, "R", expression
+
 
 def expression():
     return [equals, both, not_equals, greater, less, add, sub, mul, div, mod, either, not_rule, label, value, atom]
 
+
 def equals():
     return "BOTH", "SAEM", expression, "AN", expression
+
 
 def not_equals():
     return "DIFFRINT", expression, "AN", expression
 
+
 def both():
     return "BOTH", "OF", expression, "AN", expression
+
 
 def either():
     return "EITHER", "OF", expression, "AN", expression
 
+
 def greater():
     return "BIGGR", "OF", expression, "AN", expression
+
 
 def less():
     return "SMALLR", "OF", expression, "AN", expression
 
+
 def add():
     return "SUM", "OF", expression, "AN", expression
+
 
 def sub():
     return "DIFF", "OF", expression, "AN", expression
 
+
 def mul():
     return "PRODUKT", "OF", expression, "AN", expression
+
 
 def div():
     return "QUOSHUNT", "OF", expression, "AN", expression
 
+
 def mod():
-    return "MOD","OF", expression, "AN", expression
+    return "MOD", "OF", expression, "AN", expression
+
 
 def not_rule():
     return "NOT", expression
 
+
 def string_body():
     return _(r'[^\s"]+')
+
 
 def label():
     return _(r"\w+")
 
 
-
 def value():
-    return ["WIN", 
-    "FAIL", "NOOB", 
-    float_literal,
-    integer_literal, 
-    string_literal]
+    return ["WIN",
+            "FAIL", "NOOB",
+            float_literal,
+            integer_literal,
+            string_literal]
+
 
 def atom():
     return value
 
+
 def integer_literal():
     return _(r'\d+')
+
 
 def float_literal():
     return _(r'\d*\.\d*')
 
+
 def question_mark():
-    return ('?')
+    return '?'
+
 
 def string_literal():
     return '"', ZeroOrMore(string_body), '"'
 
+
 def kthnxbye():
     return _(r'KTHNXBYE')
 
+
 def nl():
     return _(r'\n')
+
 
 def newlines():
     return OneOrMore(nl)
@@ -130,22 +161,26 @@ def main():
     fp = open(args.input, 'r')
     content = fp.read()
     content = content.rstrip()
-    print('Hello world')
     fp.close()
-    #print(content)
-    #content = '"pokemon are"'
-    if debug:
-        parser = ParserPython(programme, ws='\t\r ', autokwd=True)
-        parse_tree = parser.parse(content)
-        print(parse_tree)
-    else:
-        try:
-            print("YAY!!!!")
-        except:
-            print(":-(")
 
-    
+    if debug:
+        try:
+            parser = ParserPython(programme, ws='\t\r ', autokwd=True)
+            parse_tree = parser.parse(content)
+            print("This is the LOLCODE you entered: \n", content)
+            print("\n")
+            print("Here's the parse tree for the entered code: ")
+            print(parse_tree, "\n")
+
+            print("Your LOLCODE syntax looks correct. Good job!")
+        except NoMatch as e:
+            print("This is the LOLCODE you entered: \n", content)
+            print("\n")
+            print("Looks like there's a syntax error in your code at line", e.line)
+    else:
+        print("You need to enter debug mode to parse your LOLCODE. \n"
+              "Try adding '-debug' to the end of your command to run the parser program.")
+
 
 if __name__ == '__main__':
     main()
-
